b8675d80090a630ad55b237558f95539f43906f2
migueltorrescosta <miguelptcosta1995@gmail.com>
2021-05-31 00:42:18 +0000

    Add proper setup

--- setup.py
+++ setup.py
@@ -1,32 +1,29 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-packages = [
-    'informathion'
-]
 
-install_requires = [
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='informathion',
+    version='0.1.6',
+    description='Measurements for Expected Opportunity Loss',
+    long_description='This package aims to provide a framework for measuring future expected opportunity losses',
+    author='migueltorrescosta',
+    author_email='miguelptcosta1995@gmail.com',
+    maintainer='migueltorrescosta',
+    maintainer_email=None,
+    url=None,
+    packages='informathion',
+    package_data=package_data,
+    install_requires=[
     'matplotlib>=3.4.2,<4.0.0',
     'numpy>=1.20.3,<2.0.0',
     'pandas>=1.2.4,<2.0.0',
     'seaborn>=0.11.1,<0.12.0'
-]
-
-setup_kwargs = {
-    'name': 'informathion',
-    'version': '0.1.6',
-    'description': 'Measurements for Expected Opportunity Loss',
-    'long_description': None,
-    'author': 'migueltorrescosta',
-    'author_email': 'miguelptcosta1995@gmail.com',
-    'maintainer': None,
-    'maintainer_email': None,
-    'url': None,
-    'packages': packages,
-    'package_data': package_data,
-    'install_requires': install_requires,
-    'python_requires': '>=3.9,<4.0',
-}
-
-
-setup(**setup_kwargs)
+    ],
+    python_requires='>=3.9,<4.0',
+)
